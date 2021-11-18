a159fa3cf1283744cde8e4be58bafa97e3e7dace
Paul Bryden <p.d.bry@outlook.com>
2021-11-17 18:17:11 -0800

    Optimized main loop to avoid multiple iterations of the rendering list

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,8 @@ mod pathfinding {
 }
 use macroquad::{
     audio::{load_sound_from_bytes, play_sound, PlaySoundParams},
+    color,
     experimental::animation::{AnimatedSprite, Animation},
-    color
 };
 mod sprites {
     pub mod engineersprite;
@@ -39,19 +39,22 @@ async fn main() {
     /*Load JSON Tilemap*/
     let lib_rs = ASSETS_DIR.get_file("tiledmap.json").unwrap();
     let body = lib_rs.contents_utf8().unwrap();
-    let map_cast: tiledmap::TiledMap = serde_json::from_str(&body).unwrap();
+    let tilemap_struct: tiledmap::TiledMap = serde_json::from_str(&body).unwrap();
     /****************************/
 
-    /*Flush first random number*/
-    rand::rand();
+    /*Seed random number generator*/
+    rand::srand((get_time() * 99999.99) as u64);
     /**************************/
+
     /*Generate Tiled Sprite List*/
-    let mut sprite_list_vector: Vec<SpriteID> = get_tilemap_spritelist(tileset, &map_cast);
-   
-    let mut selected_entity: u32 = rand::rand();
+    let mut sprite_list_vector: Vec<SpriteID> = get_tilemap_spritelist(tileset, &tilemap_struct);
+    /*Create Pathfinder*/
+    let pathfinder_local = get_pathfinder(tilemap_struct);
+    /*******************/
+
+    let mut selected_entity: u32 = 0;
     //Add Engineer Sprite
-    for i in 0..8
-    {
+    for _i in 0..8 {
         let position = vec2(rand::gen_range(1, 10) as f32, rand::gen_range(1, 10) as f32);
         let engy_sprite: SpriteID = SpriteID::Engineer(Engineer {
             texture: engineer_anim,
@@ -73,9 +76,6 @@ async fn main() {
         });
         sprite_list_vector.push(engy_sprite);
     }
-    let mut camera_movement_var = 10.; // camera movement speed multiplier.
-
-    let pathfinder_local = get_pathfinder(map_cast);
 
     play_sound(
         background_music,
@@ -87,111 +87,98 @@ async fn main() {
     let mut last_time_recorded: f64 = get_time();
     loop {
         clear_background(BLACK);
-        if camera_movement_var < 550. {
-            camera_movement_var += 0.04
-        } else {
-            camera_movement_var = 0.0; //increment camera position.
-        }
         let camera = Camera2D {
             zoom: vec2(
                 1. / macroquad::window::screen_width() * 2.,
                 -1. / macroquad::window::screen_height() * 2.,
             ),
-            target: vec2(0.0 , 100.0 ),
+            target: vec2(0.0, 100.0),
             ..Default::default()
         };
         set_camera(&camera);
 
         //Z-Index Sorting for render ordering
         sprite_list_vector.sort_by(|a, b| a.get_zindex().cmp(&b.get_zindex()));
+        /********************************/
+
+        /*Get current game clock time*/
         let current_time = get_time();
-        for sprite in sprite_list_vector.iter_mut() {
-            match sprite {
-                SpriteID::Engineer(engineer_entity) => {
-                    engineer_entity.update(current_time - last_time_recorded)
-                }
-                SpriteID::Tile(_tile_entity) => {}
-            };
-            sprite.draw(); //Draw all sprites in Sprite List
-        }
-        for sprite in sprite_list_vector.iter_mut() {
-            match sprite {
-                SpriteID::Engineer(engineer_entity) => {
-                    if(selected_entity==engineer_entity.uuid)
-                    {
-                        draw_texture_ex(
-                            selected_texture,
-                            engineer_entity.x-1.,
-                            engineer_entity.y-5.,
-                            color::WHITE,            
-                            DrawTextureParams {
-                                dest_size: Some(vec2(58.0, 64.0)),
-                                source: Some(Rect::new(
-                                    0.,
-                                    0.,
-                                    64.,
-                                    64.,
-                                )),
-                                ..Default::default()
-                            },
-                        )
-                    }
-                }
-                SpriteID::Tile(_tile_entity) => {}
-            };
-        }
-        
-        last_time_recorded = current_time;
+
+        let mut selected_texture_location: Vec2 = vec2(-1., -1.);
 
         let (mouse_x, mouse_y) = mouse_position();
+        let mut just_selected: bool = false;
+        let mut world_vec: Vec2 = vec2(-1., -1.);
+        let mut grid_coords: Vec2 = vec2(-1., -1.);
+        let mut just_clicked: bool = false;
+        let mut selected_entity_index = 0;
         if is_mouse_button_released(MouseButton::Left) {
-            println!("mouse x: {}", mouse_x);
-            println!("mouse y: {}", mouse_y);
-            println!("camera_location X: {}", camera_movement_var);
-            println!("camera_location Y: {}", camera_movement_var);
-            let world_vec = camera.screen_to_world(vec2(mouse_x, mouse_y));
+            world_vec = camera.screen_to_world(vec2(mouse_x, mouse_y));
+            grid_coords = world_to_grid_coords(world_vec);
+            just_clicked = true;
+
             println!("World X: {}", world_vec.x);
             println!("World Y: {}", world_vec.y);
-            let grid_coords = world_to_grid_coords(world_vec);
             println!("Grid X: {}", world_to_grid_coords(world_vec).x);
             println!("Grid Y: {}", world_to_grid_coords(world_vec).y);
-            // back to the original, concrete type.
-            let mut just_selected: bool = false;
-            for sprite in sprite_list_vector.iter_mut() {
-                match sprite {
-                    SpriteID::Engineer(engineer_entity) => {
+        }
+
+        for (i, sprite) in sprite_list_vector.iter_mut().enumerate() {
+            match sprite {
+                SpriteID::Engineer(engineer_entity) => {
+                    engineer_entity.update(current_time - last_time_recorded);
+                    if just_clicked {
                         if engineer_entity.is_within_bounds(world_vec) {
                             just_selected = true;
                             selected_entity = engineer_entity.uuid;
                         }
                     }
-                    SpriteID::Tile(_tile_entity) => {}
-                };
-            }
-            if (!just_selected) {
-                for sprite in sprite_list_vector.iter_mut() {
-                    match sprite {
-                        SpriteID::Engineer(engineer_entity) => {
-                            if (engineer_entity.uuid == selected_entity) {
-                                let mut path = pathfinder_local.find_path(
-                                    TilePosition {
-                                        x: engineer_entity.get_tile_pos().x as i32,
-                                        y: engineer_entity.get_tile_pos().y as i32,
-                                    },
-                                    TilePosition {
-                                        x: (grid_coords.x - 1.0) as i32,
-                                        y: (grid_coords.y - 0.5) as i32,
-                                    },
-                                );
-                                engineer_entity.update_path(std::mem::take(&mut path))
-                            }
-                        }
-                        SpriteID::Tile(_tile_entity) => {}
-                    };
+                    if selected_entity == engineer_entity.uuid {
+                        selected_texture_location =
+                            vec2(engineer_entity.x - 1., engineer_entity.y - 5.);
+                        selected_entity_index = i;
+                    }
                 }
-            }
+                SpriteID::Tile(_tile_entity) => {}
+            };
+            sprite.draw(); //Draw all sprites in Sprite List
         }
 
+        if selected_entity != 0 {
+            draw_texture_ex(
+                selected_texture,
+                selected_texture_location.x,
+                selected_texture_location.y,
+                color::WHITE,
+                DrawTextureParams {
+                    dest_size: Some(vec2(58.0, 64.0)),
+                    source: Some(Rect::new(0., 0., 64., 64.)),
+                    ..Default::default()
+                },
+            );
+        }
+        last_time_recorded = current_time;
+
+        if !just_selected && (grid_coords.x > 0.) {
+            match &mut sprite_list_vector[selected_entity_index] {
+                SpriteID::Engineer(engineer_entity) => {
+                    let mut path = pathfinder_local.find_path(
+                        TilePosition {
+                            x: engineer_entity.get_tile_pos().x as i32,
+                            y: engineer_entity.get_tile_pos().y as i32,
+                        },
+                        TilePosition {
+                            x: (grid_coords.x - 1.0) as i32,
+                            y: (grid_coords.y - 0.5) as i32,
+                        },
+                    );
+
+                    engineer_entity.update_path(std::mem::take(&mut path))
+                }
+
+                SpriteID::Tile(_tile) => {}
+            }
+        }
         next_frame().await;
     }
 }
